f1573c7dea9538b91230127de98b0351d92991f1
Emilie Karsegard <emilie.karsegard@dolagence.ch>
2017-12-15 14:43:36 +0100

    Mise en place du login

--- dev/app/providers/firebase/firebase-provider.js
+++ dev/app/providers/firebase/firebase-provider.js
@@ -14,50 +14,50 @@ export class FirebaseProvider {
   }
 
   // Auth() methode
-//   createEmailAccount(email, password){
-//     this.auth
-//             .createUserWithEmailAndPassword(email, password)
-//             .then(response => console.log(response))
-//             .catch((error)=> {
-//                 // Handle Errors here.
-//                 let errorCode = error.code;
-//                 let errorMessage = error.message;
-//                 alert(errorMessage)
-//             });
-//   }
-//   logInEmailAccount(email, password){
-//     this.auth
-//             .signInWithEmailAndPassword(email, password)
-//             .then(response => console.log(response))
-//             .catch((error)=> {
-//                 // Handle Errors here.
-//                 let errorCode = error.code;
-//                 let errorMessage = error.message;
-//                 alert(errorMessage)
-//             });
-//   }
-//
-//   // database() Methodes
-//   read(path){
-//     return this.database.ref(path)
-//   }
-//
-//   firebasePush(uid,datas){
-//     this.database.ref(this.path)
-//             .child(uid)
-//             .push(datas)
-//   }
-//
-//   firebaseUpdate(key,datas){
-//     this.database.ref(this.path)
-//             .child(key)
-//             .update(datas)
-//   }
-//
-//   firebaseSet(key,datas){
-//     this.database.ref(this.path)
-//             .child(key)
-//             .set(datas)
-//   }
-//
+  createEmailAccount(email, password){
+    this.auth
+            .createUserWithEmailAndPassword(email, password)
+            .then(response => console.log(response))
+            .catch((error)=> {
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                alert(errorMessage)
+            });
+  }
+  logInEmailAccount(email, password){
+    this.auth
+            .signInWithEmailAndPassword(email, password)
+            .then(response => console.log(response))
+            .catch((error)=> {
+                // Handle Errors here.
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                alert(errorMessage)
+            });
+  }
+
+  // database() Methodes
+  read(path){
+    return this.database.ref(path)
+  }
+
+  firebasePush(uid,datas){
+    this.database.ref(this.path)
+            .child(uid)
+            .push(datas)
+  }
+
+  firebaseUpdate(key,datas){
+    this.database.ref(this.path)
+            .child(key)
+            .update(datas)
+  }
+
+  firebaseSet(key,datas){
+    this.database.ref(this.path)
+            .child(key)
+            .set(datas)
+  }
+
 }
